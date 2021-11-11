af3a05fc4d84b20ee083348d96eadc616a8e2dca
SteveMoreira <stevemoreira2016@gmail.com>
2021-11-11 16:25:42 -0300

    Variaveis

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
-import axios from 'axios'
-
+import axios from 'axios';
+import dotenv from 'dotenv';
+dotenv.config();
 const api = axios.create({
-baseURL: 'https://tarefa-backenddd.herokuapp.com'
+baseURL: process.env.REACT_APP_BACKEND
 })
-
 export default api;
